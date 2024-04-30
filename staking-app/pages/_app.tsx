990fefb04c824c4efb972c4015e37396e1ec081d
Mat5588 <65315400+mister5588@users.noreply.github.com>
2024-04-30 04:30:42 +0200

    Update _app.tsx

--- staking-app/pages/_app.tsx
+++ staking-app/pages/_app.tsx
@@ -1,16 +1,13 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Xai } from "@thirdweb-dev/chains";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-// This is the chain your dApp will work on.
 
+// This is the chain your dApp will work on.
+const activeChain = "xai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      activeChain={Xai}
-      clientId="18360672378d10a824c58a83e95957b6"
-    >
+    <ThirdwebProvider activeChain={activeChain}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
